7917081f02919be1c2a288e08bdd8eab132346e7
AniqueCampher <ST10438712@vcconnect.edu.za>
2025-08-04 13:35:35 +0200

    Part 1 Individual

---
+++ prjIndexersICE2/TestBooks/Test1.cs
@@ -0,0 +1,46 @@
+﻿using prjIndexersICE2;
+
+namespace TestBooks
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestUpdatedPrice()
+        {
+            Books books = new Books(100, 1, "It Ends With Us", "Colleen Hoover");
+            books["price"] = 120; 
+            Assert.AreEqual(books["price"], 120);
+        }
+
+        [TestMethod]
+        public void TestUpdatedTitle()
+        {
+            Books books = new Books(150, 2, "King of Envy", "Ana Haung");
+            books[2] = "King of Greed";
+            Assert.AreEqual(books[2], "King of Greed");
+        }
+
+        [TestMethod]
+        public void TestUpdatedISBN()
+        {
+            Books books = new Books(200, 3, "Shatter Me", "Tahereh Mafi");
+            books[1] = 14;
+            Assert.AreEqual(books[1], 14);
+        }
+
+        [TestMethod]
+        public void TestUpdatedAuthorPass()
+        {
+            Books books = new Books(250, 4, "Watch Me", "Tahereh Mafi");
+            Assert.AreEqual(books["author"], "Tahereh Mafi"); 
+        }
+
+        [TestMethod]
+        public void TestUpdatedAuthorFail()
+        {
+            Books books = new Books(1, 4, "Watch Me", "Tahereh Mafi");
+            Assert.AreNotEqual(books["author"], "Shakespeare");
+        }
+    }
+}
---
+++ prjIndexersICE2/prjIndexersICE2/Books.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjIndexersICE2
+{
+    public class Books
+    {
+        int price, ISBN;
+        string title, author;
+
+        public Books(int price, int ISBN, string title, string author)
+        {
+            this.price = price;
+            this.ISBN = ISBN;
+            this.title = title;
+            this.author = author;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.price;
+                else if (index == 1)
+                    return this.ISBN;
+                else if (index == 2)
+                    return this.title;
+                else if (index == 3)
+                    return this.author;
+                return null;
+            }
+            set
+            {
+                if (index == 0)
+                    this.price = (int)value;
+                else if (index == 1)
+                    this.ISBN = (int)value;
+                else if (index == 2)
+                    this.title = (string)value;
+                else if (index == 3)
+                    this.author = (string)value;
+            }
+        }
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName == "price")
+                    return this.price;
+                else if (attrName == "ISBN")
+                    return this.ISBN;
+                else if (attrName == "title")
+                    return this.title;
+                else if (attrName == "author")
+                    return this.author;
+                return null;
+            }
+            set
+            {
+                if (attrName.ToLower().Equals("price"))
+                    this.price = (int)value;
+                else if (attrName.ToLower().Equals("ISBN"))
+                    this.ISBN = (int)value;
+                else if (attrName.ToLower().Equals("title"))
+                    this.title = (string)value;
+                else if (attrName.ToLower().Equals("author"))
+                    this.author = (string)value;
+            }
+        }
+    }
+}
